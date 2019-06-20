cc8b480e0f2352ebdbd1da9cb1e538d43ce10cf2
Marior9 <mariorodriguezan@gmail.com>
2019-06-20 13:40:27 -0400

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
+console.log("connected")
+
 export function managePresents(state, action){
+
+    // action = {
+    //     type: "INCREASE"
+    // }
+    switch (action.type) {
+        case 'INCREASE':
+        return {...state, numberOfPresents: state.numberOfPresents + 1}           
+    default:
+        return state
+    }
+
 }
